74cd8dbaae479c6e5e3204a35d8abc33e4e85339
Ammon Morris <likesalmon@gmail.com>
2020-07-24 09:12:07 -0600

    feat(plop): remove nested test

--- plopfile.js
+++ plopfile.js
@@ -1,24 +1,24 @@
 module.exports = (plop) => {
-  plop.setGenerator('module', {
-    description: 'Create a module.',
+  plop.setGenerator("module", {
+    description: "Create a module.",
     prompts: [
       {
-        type: 'input',
-        name: 'moduleName',
-        message: 'Module Name',
-        default: 'my-module',
+        type: "input",
+        name: "moduleName",
+        message: "Module Name",
+        default: "my-module",
       },
     ],
     actions: [
       {
-        type: 'add',
-        path: 'src/{{moduleName}}.ts',
-        templateFile: 'internals/plop-templates/module/module.ts.hbs',
+        type: "add",
+        path: "src/{{moduleName}}.ts",
+        templateFile: "internals/plop-templates/module/module.ts.hbs",
       },
       {
-        type: 'add',
-        path: 'src/tests/{{moduleName}}.test.ts',
-        templateFile: 'internals/plop-templates/module/module.test.ts.hbs',
+        type: "add",
+        path: "src/{{moduleName}}.test.ts",
+        templateFile: "internals/plop-templates/module/module.test.ts.hbs",
       },
     ],
   });
